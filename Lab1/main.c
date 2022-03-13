79411e67f21769523ea1fd6ba15f7968369f7119
Piotr Magiera <piotrmagiera150@gmail.com>
2022-03-13 23:49:19 +0100

    ex 1 lab1

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,7 +1,25 @@
-
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "mylib.h"
 
 int main()
 {
-    printf("essa\n");
+    int main_size = 5;
+    char** main_table = create_table(main_size);
+
+    wc_files(main_table, main_size, "elo.txt");
+    wc_files(main_table, main_size, "essa.txt");
+    wc_files(main_table, main_size, "owo.txt");
+    wc_files(main_table, main_size, "uwu.txt");
+    wc_files(main_table, main_size, "elo.txt essa.txt owo.txt uwu.txt");
+
+    int i;
+    for(i = 0; i < main_size; i++) {
+        printf("%s", main_table[i]);
+        remove_block(main_table, i);
+    }
+
+    free(main_table);
+
+    return 0;
 }
